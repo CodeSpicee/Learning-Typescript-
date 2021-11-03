3a409be4f1c6a431670c5b4817c550e5f3153c19
unknown <afolabibidemi63@yahoo.com>
2021-11-03 10:29:25 +0100

    learning typecript

---
+++ main.js
@@ -0,0 +1,19 @@
+// let message: string = "Hello Word"
+// let heading = document.createElement("h1")
+// heading.textContent = message
+// document.body.appendChild(heading)
+var Person = /** @class */ (function () {
+    function Person(firstName, lastName, age, country, hobbies) {
+        var _this = this;
+        this.addName = function () { return _this.firstName.concat(" " + _this.lastName); };
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.country = country;
+        this.hobbies = hobbies;
+    }
+    return Person;
+}());
+var person1 = new Person("Jamiu", "Afolabi", 24 + 1, "Nigeria", ['dancing', 'travelling', 'writing', 'seeing movies', 'playing games']);
+console.log(person1);
+console.log(person1.addName());
---
+++ main.ts
@@ -0,0 +1,44 @@
+// let message: string = "Hello Word"
+// let heading = document.createElement("h1")
+// heading.textContent = message
+// document.body.appendChild(heading)
+
+
+// function getArray<T>(items: T[]): T[] {
+// return new Array().concat(items)
+// }
+ 
+// let numArray  = getArray<number>([1,2,34+1])
+// let stringArray = getArray<string>(['name', 'bolu', '5'])
+// console.log(stringArray)
+// console.log(numArray)
+
+interface PersonInterface {
+    firstName: string;
+    lastName: string;
+    age: number;
+    country: string;
+    hobbies: string[]
+    addName(): string
+}
+
+class Person implements PersonInterface {
+    firstName: string;
+    lastName: string;
+    age: number;
+    country: string;
+    hobbies: string[]
+    
+    constructor(firstName: string, lastName: string, age: number, country: string, hobbies: string[]) {
+        this.firstName = firstName
+        this.lastName = lastName
+        this.age = age
+        this.country = country
+        this.hobbies = hobbies
+    }
+    addName = () => this.firstName.concat(" " + this.lastName)
+}
+
+let person1 = new Person("Jamiu", "Afolabi", 24+1, "Nigeria", ['dancing', 'travelling', 'writing', 'seeing movies', 'playing games'])
+console.log(person1)
+console.log(person1.addName())
